997e4548fc42fb1f504bfb4820a6ff97ff81d808
ZiAng <ziang.rice@gmail.com>
2019-03-16 00:23:05 -0500

    heroku ready

--- StarterCode/Belly_Button_Biodiversity/app.py
+++ StarterCode/Belly_Button_Biodiversity/app.py
@@ -18,7 +18,8 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite" # for local
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_1', '') # for heroku
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
